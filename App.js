e94903b06836fd1284778081e028e9fe81ffd6d0
John Necir Rebellion <johnnecirrebellion@gmail.com>
2023-12-13 07:56:07 +0800

    Fix WIP

--- App.js
+++ App.js
@@ -1,141 +1,111 @@
-import React, { useState, useEffect, useRef } from "react";
-import { View, Text, TouchableOpacity, Alert, TextInput } from "react-native";
-import { Camera, getSupportedRatiosAsync } from "expo-camera";
+import React, { useState, useEffect, useRef } from "react"
+import { View, Text, TouchableOpacity } from "react-native"
+import { Camera } from "expo-camera"
 import {
   GestureHandlerRootView,
   PinchGestureHandler,
-} from "react-native-gesture-handler";
-import { manipulateAsync, SaveFormat } from "expo-image-manipulator";
+} from "react-native-gesture-handler"
+import { manipulateAsync, SaveFormat } from "expo-image-manipulator"
+import * as Speech from "expo-speech"
 
 export default function App() {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
-  const [socket, setSocket] = useState(null);
-  const [webSocketURL, setWebSocketURL] = useState('localhost:8000')
+  const [hasPermission, setHasPermission] = useState(null)
+  const [cameraType, setCameraType] = useState(Camera.Constants.Type.back)
+  const [text, setText] = useState("Scan")
 
-  const cameraRef = useRef(null);
+  const cameraRef = useRef(null)
 
   useEffect(() => {
-    (async () => {
-      const { status } = await Camera.requestCameraPermissionsAsync();
-      setHasPermission(status === "granted");
-      initializeWebSocket()
-    })();
-  }, []);
-
-  const initializeWebSocket = () => {
-    Alert.alert(webSocketURL)
-    const ws = new WebSocket(
-      `ws://${webSocketURL}/ws/1`,
-    ); // Replace with your WebSocket server URL
-    setSocket(ws);
-
-    ws.onopen = () => {
-      Alert.alert('webSocketURL connected!')
-    };
-    // Listen for messages
-    ws.onmessage = (event) => {
-      const message = event.data;
-      console.log(message);
-      alert(message);
-    };
-
-    // Connection closed
-    ws.onclose = () => {
-      console.log("<p>WebSocket connection closed</p>");
-    };
-
-    // Connection error
-    ws.onerror = () => {
-      console.log("<p>Error in WebSocket connection</p>");
-    };
-  }
-
+    ;(async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync()
+      setHasPermission(status === "granted")
+    })()
+  }, [])
 
   const handleCapture = async () => {
     if (cameraRef.current) {
       let photo = await cameraRef.current.takePictureAsync({
-        ratio: "19:9",
-        quality: 0,
-        type: "jpg",
-        base64: true,
-      });
-      if (socket) {
-        const manipResult = await manipulateAsync(
-          photo.localUri || photo.uri,
-          [{ resize: { width: 1280, height: 720 } }],
-          { base64: true, compress: 0, format: SaveFormat.JPEG },
-        );
+        type: "png",
+      })
+      const manipResult = await manipulateAsync(
+        photo.localUri || photo.uri,
+        [],
+        { compress: 0.65, format: SaveFormat.JPEG }
+      )
+      const localUri = manipResult.uri
+      let filename = localUri.split("/").pop()
 
-        alert("Loading...");
-        await socket.send(`data:image/jpeg;base64,${manipResult.base64}`);
-      }
+      // Infer the type of the image
+      let match = /\.(\w+)$/.exec(filename)
+      let type = match ? `image/${match[1]}` : `image`
+
+      // Upload the image using the fetch and FormData APIs
+      let formData = new FormData()
+      // Assume "photo" is the name of the form field the server expects
+      formData.append("image", { uri: localUri, name: filename, type })
+      fetch("http://192.168.0.106:8000/", {
+        method: "POST",
+        headers: {
+          "content-type": "multipart/form-data",
+        },
+        body: formData,
+      })
+        .then((res) => res.text())
+        .then((data) => {
+          setText(data)
+          Speech.speak(data)
+        })
+        .finally(() => {
+          setTimeout(()=>setText("Scan"), 3000)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+      setText("Scanning...")
+      Speech.speak("Scanning...")
     }
-  };
+  }
 
   const handleZoom = (event) => {
     // Handle pinch gesture for zoomingprompt
     // You can customize this based on your requirements
     // Check the event.nativeEvent.scale for pinch scale value
-  };
-
+  }
 
   if (hasPermission === null) {
-    return <View />;
+    return <View />
   }
   if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
+    return <Text>No access to camera</Text>
   }
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      {/* <View style={{ padding: 10, marginTop: 30 }}>
-        <TextInput onChangeText={url => { setWebSocketURL(url) }}></TextInput>
-        <TouchableOpacity onPress={() => initializeWebSocket()}>
-          <Text style={{ color: 'black' }}>Change WebSocket URL</Text>
-        </TouchableOpacity>
-      </View> */}
-
       <Camera style={{ flex: 1 }} type={cameraType} ref={cameraRef}>
         <PinchGestureHandler onGestureEvent={handleZoom}>
           <View
             style={{
               flex: 1,
-              justifyContent: "flex-end",
               alignItems: "center",
             }}
           >
-            <TouchableOpacity
-              style={{ marginBottom: 30 }}
-              onPress={handleCapture}
-            >
+            <TouchableOpacity onPress={handleCapture}>
               <View
                 style={{
-                  borderWidth: 2,
-                  borderRadius: 50,
-                  borderColor: "white",
-                  height: 50,
-                  width: 50,
+                  height: 999,
+                  width: 999,
                   display: "flex",
                   justifyContent: "center",
                   alignItems: "center",
+                  backgroundColor: "transparent",
                 }}
               >
-                <View
-                  style={{
-                    borderWidth: 2,
-                    borderRadius: 50,
-                    borderColor: "white",
-                    height: 40,
-                    width: 40,
-                    backgroundColor: "white",
-                  }}
-                />
+                <Text style={{fontSize: 30, color: 'white'}}>{text}</Text>
               </View>
             </TouchableOpacity>
           </View>
         </PinchGestureHandler>
       </Camera>
     </GestureHandlerRootView>
-  );
+  )
 }
