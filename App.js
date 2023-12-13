01de8616717da337088034b9154ccc3802593a63
John Necir Rebellion <johnnecirrebellion@gmail.com>
2023-12-13 13:34:18 +0800

    New UI update

--- App.js
+++ App.js
@@ -4,14 +4,17 @@ import { Camera } from "expo-camera"
 import {
   GestureHandlerRootView,
   PinchGestureHandler,
+  PanGestureHandler,
 } from "react-native-gesture-handler"
 import { manipulateAsync, SaveFormat } from "expo-image-manipulator"
 import * as Speech from "expo-speech"
+import { LinearGradient } from "expo-linear-gradient"
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null)
   const [cameraType, setCameraType] = useState(Camera.Constants.Type.back)
-  const [text, setText] = useState("Scan")
+  const [text, setText] = useState()
+  const [titleShown, setTitleShown] = useState(true)
 
   const cameraRef = useRef(null)
 
@@ -19,11 +22,26 @@ export default function App() {
     ;(async () => {
       const { status } = await Camera.requestCameraPermissionsAsync()
       setHasPermission(status === "granted")
+
+      if (status === "granted") {
+        setText("Fake Money Detector")
+      }
     })()
   }, [])
 
+  useEffect(() => {
+    if (text) {
+      Speech.speak(text)
+
+      if (text === "Fake Money Detector") {
+        Speech.speak("swipe up")
+      }
+    }
+  }, [text])
+
   const handleCapture = async () => {
     if (cameraRef.current) {
+      setText("Scanning...")
       let photo = await cameraRef.current.takePictureAsync({
         type: "png",
       })
@@ -53,23 +71,19 @@ export default function App() {
         .then((res) => res.text())
         .then((data) => {
           setText(data)
-          Speech.speak(data)
         })
         .finally(() => {
-          setTimeout(()=>setText("Scan"), 3000)
+          setTimeout(() => setText("Touch to scan"), 3000)
         })
         .catch((err) => {
           console.log(err)
         })
-      setText("Scanning...")
-      Speech.speak("Scanning...")
     }
   }
 
-  const handleZoom = (event) => {
-    // Handle pinch gesture for zoomingprompt
-    // You can customize this based on your requirements
-    // Check the event.nativeEvent.scale for pinch scale value
+  const onPanGestureEvent = (event) => {
+    setTitleShown(false)
+    setText("Touch to scan")
   }
 
   if (hasPermission === null) {
@@ -81,31 +95,69 @@ export default function App() {
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Camera style={{ flex: 1 }} type={cameraType} ref={cameraRef}>
-        <PinchGestureHandler onGestureEvent={handleZoom}>
-          <View
+      <PanGestureHandler onGestureEvent={onPanGestureEvent}>
+        {titleShown ? (
+          <LinearGradient
             style={{
               flex: 1,
+              display: "flex",
+              justifyContent: "center",
               alignItems: "center",
             }}
+            colors={["#0008C3", "#006AB7"]}
           >
-            <TouchableOpacity onPress={handleCapture}>
-              <View
+            <View
+              style={{
+                flex: 1,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Text
+                style={{ fontSize: 56, textAlign: "center", color: "white" }}
+              >
+                Fake Money Detector
+              </Text>
+              <Text
                 style={{
-                  height: 999,
-                  width: 999,
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  backgroundColor: "transparent",
+                  fontSize: 24,
+                  textAlign: "center",
+                  color: "white",
+                  bottom: 10,
+                  position: "absolute",
                 }}
               >
-                <Text style={{fontSize: 30, color: 'white'}}>{text}</Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-        </PinchGestureHandler>
-      </Camera>
+                swipe up
+              </Text>
+            </View>
+          </LinearGradient>
+        ) : (
+          <Camera style={{ flex: 1 }} type={cameraType} ref={cameraRef}>
+            <View
+              style={{
+                flex: 1,
+                alignItems: "center",
+              }}
+            >
+              <TouchableOpacity onPress={handleCapture}>
+                <View
+                  style={{
+                    height: 999,
+                    width: 999,
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    backgroundColor: "transparent",
+                  }}
+                >
+                  <Text style={{ fontSize: 30, color: "white" }}>{text}</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+          </Camera>
+        )}
+      </PanGestureHandler>
     </GestureHandlerRootView>
   )
 }
