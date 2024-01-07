f8979d9a37882b8cf7ff6ca192b2e9307a274161
John Necir Rebellion <johnnecirrebellion@gmail.com>
2024-01-07 22:03:25 +0800

    Update build.gradle

--- app.config.js
+++ app.config.js
@@ -1,8 +1,9 @@
-
-
 export default {
-  name: 'BillDetect',
-  plugins: [
-    "./fix.plugin.js",
-  ],
-};
+  name: "BillDetect",
+  plugins: ["./fix.plugin.js"],
+  extra: {
+    eas: {
+      projectId: "646d0886-d603-4886-8d1e-fa83ceab9924",
+    },
+  },
+}
